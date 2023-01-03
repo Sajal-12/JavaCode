83b069aed95f5cb911f08759a57902c074dba142
SAJAL SWAPNIL <sajalvictorious83@gmail.com>
2023-01-03 14:34:44 +0530

    Updated Files

---
+++ MyApplication.java
@@ -0,0 +1,16 @@
+package AppData;
+import java.util.Scanner;
+public class MyApplication{
+
+	public static void main(String[] args){
+		
+		Scanner input = new Scanner(System.in);
+		int number = 0; 
+		do{
+		System.out.println("Enter a number: ");
+		number = input.nextInt();
+		}
+		while(number != 9);
+		System.out.println("You got 9!");
+	}
+}
